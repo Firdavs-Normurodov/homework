407f929dea47f8d60167250905c2afdb1460852e
normurodovfirdavs <108564450+Firdavs-Normurodov@users.noreply.github.com>
2025-04-08 21:54:35 +0500

    update

--- src/js/switcher.js
+++ src/js/switcher.js
@@ -1,12 +1,11 @@
 "use strict";
 const switcher = document.querySelector(".switcher");
+
 switcher.addEventListener("click", () => {
-  const isCurrentlyDark = switcher.classList.contains("fa-moon");
-  if (isCurrentlyDark) {
-    document.body.style.setProperty("--background", "#fff");
-    switcher.classList.replace("fa-moon", "fa-sun");
-  } else {
-    document.body.style.setProperty("--background", "#000");
+  document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
     switcher.classList.replace("fa-sun", "fa-moon");
+  } else {
+    switcher.classList.replace("fa-moon", "fa-sun");
   }
 });
